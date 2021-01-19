4cf7d2538749d3c96679273b077bb5b8b7862f22
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-20 01:09:07 +0530

    ADD unit tests for Conditions.js

--- src/components/Conditions/__test__/Conditions.test.js
+++ src/components/Conditions/__test__/Conditions.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Conditions from './../Conditions';
+import { render,cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 
+
+afterEach(cleanup);
 
 it ("renders without crashing", () => {
     var responseObj={message:"Just Basic Testing", cod:300};
+    var error = true;
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <Conditions error responseObj></Conditions>,div);
+})
+
+it ("renders without crashing", () => {
+    var responseObj={message:"city not found", cod:200};
+    var error = false;
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <Conditions error responseObj></Conditions>,div);
+})
+
+it ("renders without crashing", () => {
+    var responseObj={message:"city not found", cod:200};
+    var error = false;
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <Conditions error responseObj></Conditions>,div);
+})
+
+it ("renders without crashing", () => {
+    var responseObj={message:"city not found", cod:200};
     var error = true;
     const div = document.createElement("div");
     ReactDOM.render(
