adb2e33887997ec62450605a48c24fc0fa38d8da
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-20 10:00:36 +0530

    Add test for wrong word for the app

--- src/components/Conditions/__test__/Conditions.test.js
+++ src/components/Conditions/__test__/Conditions.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Conditions from './../Conditions';
-import { render,cleanup } from '@testing-library/react';
+import { render,cleanup,getByTestId } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
 
@@ -15,26 +15,32 @@ it ("renders without crashing", () => {
         <Conditions error responseObj></Conditions>,div);
 })
 
-it ("renders without crashing", () => {
-    var responseObj={message:"city not found", cod:200};
-    var error = false;
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <Conditions error responseObj></Conditions>,div);
-})
+// it ("renders without crashing", () => {
+//     var responseObj={message:"city not found", cod:200};
+//     var error = false;
+//     const div = document.createElement("div");
+//     ReactDOM.render(
+//         <Conditions error responseObj></Conditions>,div);
+// })
 
-it ("renders without crashing", () => {
-    var responseObj={message:"city not found", cod:200};
-    var error = false;
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <Conditions error responseObj></Conditions>,div);
-})
+// it ("renders without crashing", () => {
+//     var responseObj={message:"city not found", cod:200};
+//     var error = false;
+//     const div = document.createElement("div");
+//     ReactDOM.render(
+//         <Conditions error responseObj></Conditions>,div);
+// })
 
-it ("renders without crashing", () => {
-    var responseObj={message:"city not found", cod:200};
-    var error = true;
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <Conditions error responseObj></Conditions>,div);
+// it ("renders without crashing", () => {
+//     var responseObj={message:"city not found", cod:200};
+//     var error = true;
+//     const div = document.createElement("div");
+//     ReactDOM.render(
+//         <Conditions error responseObj></Conditions>,div);
+// })
+
+it("checks for port", () => {
+    var responseObj={message:"Just Basic Testing", cod:300};
+    const {getByTestId} = render(<Conditions responseObj={"Just Basic Testing", 300}></Conditions>);
+    expect(getByTestId('test')).toHaveTextContent(responseObj.message);
 })
